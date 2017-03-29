5ee90c2cc182b10cc41c1f251766c03761868e8d
T.Tomita <ttp8101@gmail.com>
2017-03-29 18:55:08 +0900

    Change spec name

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'User', type: :feature do
+RSpec.feature 'User management', type: :feature do
   scenario "adds a new user" do
     visit root_path
     click_link "Sign up"
@@ -15,44 +15,44 @@ RSpec.feature 'User', type: :feature do
     expect(current_path).to eq user_path(User.last)
     expect(page).to have_content("Your registration is successful")
   end
+end
 
-  context "with logged in user" do
-    before :each do
-      @user = create(:user)
-      log_in @user
-    end
+RSpec.feature "Logged in user" do
+  before :each do
+    @user = create(:user)
+    log_in @user
+  end
 
-    scenario "updates a user profile" do
-      visit root_path
-      click_link "Settings"
-      
-      fill_in "Name", with: "updated user name"
-      fill_in "Password", with: @user.password
-      fill_in "Confirmation", with: @user.password
-      click_button "Update"
+  scenario "updates a user profile" do
+    visit root_path
+    click_link "Settings"
+    
+    fill_in "Name", with: "updated user name"
+    fill_in "Password", with: @user.password
+    fill_in "Confirmation", with: @user.password
+    click_button "Update"
 
-      expect(current_path).to eq user_path(@user)
-      expect(page).to have_content("Profile updated")
-      expect(page).to have_content("updated user name")
-    end
+    expect(current_path).to eq user_path(@user)
+    expect(page).to have_content("Profile updated")
+    expect(page).to have_content("updated user name")
+  end
 
-    scenario "can not update other user's profile" do
-      other_user = create(:user, name: "andrew")
-      visit edit_user_path(other_user)
-      expect(current_path).to eq root_path
-      expect(page).to have_content("Permission denied")
-    end
+  scenario "can not update other user's profile" do
+    other_user = create(:user, name: "andrew")
+    visit edit_user_path(other_user)
+    expect(current_path).to eq root_path
+    expect(page).to have_content("Permission denied")
+  end
 
-    scenario "creates new topics" do
-      visit root_path
+  scenario "creates new topics" do
+    visit root_path
 
-      expect {
-        fill_in "Topic Title", with: "Lorem ipsum"
-        click_button "Create"
-      }.to change(Topic, :count).by(1)
+    expect {
+      fill_in "Topic Title", with: "Lorem ipsum"
+      click_button "Create"
+    }.to change(Topic, :count).by(1)
 
-      expect(current_path).to eq topic_path(Topic.last)
-      expect(page).to have_content("Your topic is created")
-    end
+    expect(current_path).to eq topic_path(Topic.last)
+    expect(page).to have_content("Your topic is created")
   end
 end
